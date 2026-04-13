16925bc71d7ebcd2d04084325e60efaf8054880a
Tomas <tburculosis@protonmail.com>
2026-04-13 22:04:51 +0100

    can now navigate forwards and backwards in search

--- polip.c
+++ polip.c
@@ -422,14 +422,35 @@ void editorSave() {
 
 //***find***//
 void editorFindCallback(char *query, int key) {
-    if (key == '\r' || key == '\x1b') return;
+    static int last_match = -1;
+    static int direction = 1;
 
+    if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
+        return; 
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
+    }
+
+    if (last_match == -1) direction = 1;
+    int current = last_match;
     int i;
     for ( i = 0; i < E.numrows; i++) {
-        erow *row = &E.row[i];
+        current += direction;
+        if (current == -1) current = E.numrows - 1;
+        else if (current == E.numrows) current = 0;
+
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query);
         if (match) {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowOffset = E.numrows;
             break;
