278cc8f644519972649cc61b50722af24aeb22d0
Tomas <tburculosis@protonmail.com>
2026-04-21 16:46:57 +0100

    exiting a search now unhighlights the match and decimal numbers are included in number highlighting

--- polip.c
+++ polip.c
@@ -198,23 +198,36 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 //***syntax highlighting***//
+int is_separator(int c) {
+    return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
 
 void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
-    int i;
-    for (i = 0; i < row->rsize; i++) {
-        if (isdigit(row->render[i])) {
+    int prev_sep = 1;
+
+    int i = 0;
+    while (i < row->rsize) {
+        char c = row->render[i];
+        unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+            
+        if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c = '.' && prev_hl == HL_NUMBER)) {
             row->hl[i] = HL_NUMBER;
+            i++;
+            prev_sep = 0;
+            continue;
         }
+        prev_sep = is_separator(c);
+        i++;
     }
 }
 
 int editorSyntaxToColour(int hl) {
     switch (hl) {
-        case HL_NORMAL: 
-            return 31;
+        case HL_NUMBER: 
+            return 31; 
         case HL_MATCH:
             return 34;
         default: return 37;
@@ -459,6 +472,15 @@ void editorFindCallback(char *query, int key) {
     static int last_match = -1;
     static int direction = 1;
 
+    static int saved_hl_line;
+    static char *saved_hl = NULL;
+
+    if (saved_hl) {
+        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+
     if (key == '\r' || key == '\x1b') {
         last_match = -1;
         direction = 1;
@@ -488,6 +510,9 @@ void editorFindCallback(char *query, int key) {
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowOffset = E.numrows;
 
+            saved_hl_line = current;
+            saved_hl = malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
             memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
